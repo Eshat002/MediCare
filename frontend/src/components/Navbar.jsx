fc86acd2552943f58082adc63293e3ffc4cfe4fa
eshat002 <eshatjubayer22@outlook.com>
2024-09-12 21:22:22 +0600

    Made Navbar responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,13 +11,13 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="px-10 md:px-20 py-5">
+    <nav className="px-8 lg:px-14 py-5">
       <div className="container mx-auto flex items-center justify-between">
         <div className="text-white">
           <img width={150} src={logo} alt="logo" />
         </div>
 
-        <div className="hidden md:flex space-x-8">
+        <div className="hidden lg:flex space-x-8">
           <a
             href="#"
             className="text-link-color text-base hover:text-sky-400 capitalize"
@@ -50,13 +50,13 @@ const Navbar = () => {
           </a>
         </div>
         {/* Mobile menu icon */}
-        <div className="md:hidden">
+        <div className="lg:hidden">
           <button onClick={toggleMenu} className="text-2xl">
             {isOpen ? <FaTimes /> : <FaBars />} {/* Icon toggles */}
           </button>
         </div>
         {/* Make Appointment button */}
-        <div className="hidden md:block">
+        <div className="hidden lg:block">
           <button className="bg-primary text-white px-6 py-2 rounded-full hover:bg-sky-500">
             Make Appointment
           </button>
@@ -65,13 +65,13 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       <div
-        className={`md:hidden bg-white flex flex-col space-y-4 p-4 transition ease-linear duration-300 ${
+        className={`lg:hidden bg-white flex flex-col space-y-4 py-4 transition ease-linear md:text-center duration-300 ${
           isOpen ? "block" : "hidden"
         }`}
       >
         <a
           href="#"
-          className="text-link-color text-base hover:text-sky-400 capitalize"
+          className="text-link-color pt-2 text-base hover:text-sky-400 capitalize"
         >
           Home
         </a>
@@ -95,7 +95,7 @@ const Navbar = () => {
         </a>
         <a
           href="#"
-          className="text-link-color text-base hover:text-sky-400 capitalize"
+          className="text-link-color text-base pb-2 hover:text-sky-400 capitalize"
         >
           Blog
         </a>
