c284a2ad0296a0c2084be098c5c10730df2e4795
eshat002 <eshatjubayer22@outlook.com>
2024-09-21 19:09:31 +0600

    Add shadow to button

--- frontend/src/components/BtnWithIcon.jsx
+++ frontend/src/components/BtnWithIcon.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BtnWithIcon = ({ text, icon }) => {
   return (
-    <button className="bg-primary text-lg font-normal flex items-center text-white px-6 py-3 rounded-full shadow-md hover:bg-sky-500">
+    <button className="bg-primary shadow-lg shadow-primary/35 text-lg font-normal flex items-center text-white px-6 py-3 rounded-full hover:bg-sky-500">
       <span className="me-2">{text}</span>
       {icon}
     </button>
