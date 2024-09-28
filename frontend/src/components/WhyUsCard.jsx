f3335ee0a3b8041fedc5d96200cd1c402a15dd98
eshat002 <eshatjubayer22@outlook.com>
2024-09-28 18:50:16 +0600

    Pass data to whyUsCard

--- frontend/src/components/WhyUsCard.jsx
+++ frontend/src/components/WhyUsCard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const WhyUsCard = ({ img, title, text }) => {
+const WhyUsCard = ({ img, title, text, customClass }) => {
   return (
-    <div className="whyUsCard">
+    <div className={`${customClass}  whyUsCard mb-14 px-8 py-12 rounded-3xl`}>
       <img className="w-20 h-auto object-cover" src={img} alt="DNA image" />
-      <h5>{title}</h5>
-      <p>{text}</p>
+      <h5 className="text-2xl text-primaryBlack font-semibold mt-5 mb-2 capitalize">
+        {title}
+      </h5>
+      <p className="text-base text-primaryBlack/70 font-normal ">{text}</p>
     </div>
   );
 };
