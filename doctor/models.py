5e91e31e539cbfe01df63df8fb4256c217369fa7
eshat002 <eshatjubayer22@outlook.com>
2024-10-04 19:48:18 +0600

    add happy_clients and doctor_at fields to Doctor Model

--- doctor/models.py
+++ doctor/models.py
@@ -20,13 +20,15 @@ class Doctor(models.Model):
 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    avatar = models.ImageField(upload_to="doctors/avatars/")
+    avatar = models.ImageField(upload_to="doctors/avatars/", default="doctors/avatars/doctor.png")
     specialization = models.CharField(max_length=50, choices=SPECIALIZATION_CHOICES)
     years_of_experience = models.PositiveIntegerField()
     email = models.EmailField()
     phone = models.CharField(max_length=15)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
     available = models.BooleanField(default=True)
+    happy_clients = models.PositiveIntegerField()
+    doctor_at = models.CharField(max_length=100, default="iMedical Central Clinic, LA")
     date_joined = models.DateTimeField(auto_now_add=True)
 
     class Meta:
