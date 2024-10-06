9a19f81506eb1252db8e1864b5a00042276fb86d
eshat002 <eshatjubayer22@outlook.com>
2024-10-06 20:37:28 +0600

    Add Doctor and DoctorCard component

---
+++ frontend/src/components/DoctorCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DoctorCard = () => {
+  return <div>doctorCard</div>;
+};
+
+export default DoctorCard;
--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Hero from "../components/Hero";
 import Service from "../components/Service";
 import WhyUs from "../components/WhyUs";
+import Test from "../components/Test";
+import Doctors from "../components/Doctors";
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero />
       <Service />
       <WhyUs />
+      <Doctors />
+      {/* <Test /> */}
     </div>
   );
 };
