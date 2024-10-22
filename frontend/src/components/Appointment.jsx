c50e0733cc8a3e67e06c030a4e77437d009dd694
eshat002 <eshatjubayer22@outlook.com>
2024-10-22 18:44:28 +0600

    Add modal animation

--- frontend/src/components/Appointment.jsx
+++ frontend/src/components/Appointment.jsx
@@ -22,6 +22,7 @@ const Appointment = () => {
 
   const [errors, setErrors] = useState({}); // State to capture errors from the backend
   const [isSuccessModalOpen, setIsSuccessModalOpen] = useState(false); // State for success modal
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -52,6 +53,10 @@ const Appointment = () => {
         appointment_date_time: null,
         message: "",
       });
+      setTimeout(() => {
+        setIsModalVisible(true); // Trigger the animation
+      }, 50);
+
       setIsSuccessModalOpen(true);
     } catch (error) {
       if (error.response && error.response.data) {
@@ -198,8 +203,8 @@ const Appointment = () => {
           </div>
         </form>
       </div>
-      {isSuccessModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-800 bg-opacity-50 transition-all duration-300 ease-in-out">
+      {/* {isSuccessModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-800 bg-opacity-50 transition-opacity duration-300 ">
           <div className="bg-white p-6 rounded-lg shadow-lg">
             <h2 className="text-xl font-bold mb-4">Appointment Created!</h2>
             <p className="mb-4">
@@ -211,175 +216,40 @@ const Appointment = () => {
             />
           </div>
         </div>
+      )} */}
+      {isSuccessModalOpen && (
+        <div
+          className={`fixed inset-0 flex items-center justify-center z-50 bg-gray-800 bg-opacity-50 transition-opacity duration-300 ${
+            isModalVisible ? "opacity-100" : "opacity-0"
+          }`}
+          style={{
+            transition: "opacity 0.3s ease",
+            visibility: isModalVisible ? "visible" : "hidden",
+          }}
+        >
+          <div
+            className={`bg-white p-6 rounded-lg shadow-lg transition-transform duration-300 transform ${
+              isModalVisible ? "translate-y-0" : "translate-y-4"
+            }`}
+          >
+            <h2 className="text-xl font-bold mb-4">Appointment Created!</h2>
+            <p className="mb-4">
+              Your appointment has been successfully created.
+            </p>
+            <BtnWithoutIcon
+              onClick={() => {
+                setIsModalVisible(false); // Start the fade-out
+                setTimeout(() => {
+                  setIsSuccessModalOpen(false); // Hide the modal after fade-out
+                }, 300); // Match this timeout with the fade-out duration
+              }}
+              text="Close"
+            />
+          </div>
+        </div>
       )}
     </section>
   );
 };
 
 export default Appointment;
-
-// import React, { useState } from "react";
-// import Flatpickr from "react-flatpickr";
-// import "flatpickr/dist/flatpickr.min.css"; // Import Flatpickr CSS
-// import CalenderGray from "../assets/svg/CalenderGray.jsx";
-// import SectionHeadline from "./SectionHeadline.jsx";
-// import BtnWithIcon from "./BtnWithIcon.jsx";
-// import { FaTelegramPlane } from "react-icons/fa";
-// import axios from "axios";
-
-// const BaseUrl = import.meta.env.VITE_API_URL;
-
-// const Appointment = () => {
-//   const [formData, setFormData] = useState({
-//     first_name: "",
-//     last_name: "",
-//     email: "",
-//     phone: "",
-//     appointment_date_time: null, // Combined date and time
-//     message: "",
-//   });
-
-//   const handleChange = (e) => {
-//     setFormData({ ...formData, [e.target.name]: e.target.value });
-//   };
-
-//   const handleDateTimeChange = (selectedDates) => {
-//     setFormData({ ...formData, appointment_date_time: selectedDates[0] }); // Set combined date and time
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     console.log(formData);
-
-//     try {
-//       const response = await axios.post(
-//         `${BaseUrl}/api/appointments/create/`,
-//         formData
-//       );
-
-//       console.log("Appointment Created:", response.data);
-
-//       // Reset form data
-//       setFormData({
-//         first_name: "",
-//         last_name: "",
-//         email: "",
-//         phone: "",
-//         appointment_date_time: null,
-//         message: "",
-//       });
-//     } catch (error) {
-//       console.error(
-//         "There was an error creating the appointment!",
-//         error.response.data
-//       );
-//     }
-//   };
-
-//   return (
-//     <section className="bg-white flex flex-col items-center justify-center py-20">
-//       <div className="section-headline-container mb-5">
-//         <SectionHeadline text="Make Appointment" />
-//       </div>
-//       <div className="p-6 w-full max-w-3xl">
-//         <form onSubmit={handleSubmit}>
-//           {/* First Name and Last Name */}
-//           <div className="grid grid-cols-2 gap-7 mb-7">
-//             <div>
-//               <input
-//                 className="bg-[#fcfcfc] placeholder:text-primaryBlack/60 placeholder:text-base placeholder:font-medium appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-primary"
-//                 id="first_name"
-//                 name="first_name" // Changed to match formData
-//                 type="text"
-//                 placeholder="First Name"
-//                 value={formData.first_name} // Changed to match formData
-//                 onChange={handleChange}
-//                 required
-//               />
-//             </div>
-//             <div>
-//               <input
-//                 className="bg-[#FCFCFC] placeholder:text-primaryBlack/60 placeholder:text-base placeholder:font-medium appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-primary"
-//                 id="last_name"
-//                 name="last_name" // Changed to match formData
-//                 type="text"
-//                 placeholder="Last Name"
-//                 value={formData.last_name} // Changed to match formData
-//                 onChange={handleChange}
-//                 required
-//               />
-//             </div>
-//           </div>
-
-//           {/* Email and Phone */}
-//           <div className="grid grid-cols-2 gap-7 mb-7">
-//             <div>
-//               <input
-//                 className="bg-[#FCFCFC] placeholder:text-primaryBlack/60 placeholder:text-base placeholder:font-medium appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-primary"
-//                 id="email"
-//                 name="email"
-//                 type="email"
-//                 placeholder="Email"
-//                 value={formData.email}
-//                 onChange={handleChange}
-//                 required
-//               />
-//             </div>
-//             <div>
-//               <input
-//                 className="bg-[#FCFCFC] placeholder:text-primaryBlack/60 placeholder:text-base placeholder:font-medium appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-primary"
-//                 id="phone"
-//                 name="phone"
-//                 type="tel"
-//                 placeholder="Phone"
-//                 value={formData.phone}
-//                 onChange={handleChange}
-//                 required
-//               />
-//             </div>
-//           </div>
-
-//           {/* Appointment Date and Time using Flatpickr */}
-//           <div className="relative mb-7">
-//             <div className="absolute left-3 top-3 text-gray-500 pointer-events-none">
-//               <CalenderGray />
-//             </div>
-//             <Flatpickr
-//               className="bg-[#FCFCFC] placeholder:text-primaryBlack/60 placeholder:text-base placeholder:font-medium appearance-none border rounded-lg w-full py-3 px-12 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-primary"
-//               id="appointment_date_time" // Changed to match formData
-//               options={{
-//                 enableTime: true,
-//                 dateFormat: "Y-m-d H:i", // Set the date and time format
-//                 time_24hr: false, // Use 12-hour time
-//               }}
-//               value={formData.appointment_date_time} // Changed to match formData
-//               onChange={handleDateTimeChange}
-//               placeholder="Appointment Date and Time" // Placeholder text
-//               required
-//             />
-//           </div>
-
-//           {/* Message */}
-//           <div className="mb-8">
-//             <textarea
-//               className="bg-[#FCFCFC] placeholder:text-primaryBlack/60 placeholder:text-base placeholder:font-medium appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-primary"
-//               id="message"
-//               name="message"
-//               placeholder="Describe what you’re looking for..."
-//               value={formData.message}
-//               onChange={handleChange}
-//               rows="5"
-//             />
-//           </div>
-
-//           {/* Submit Button */}
-//           <div className="flex items-center justify-center">
-//             <BtnWithIcon text="Send" icon={<FaTelegramPlane size={20} />} />
-//           </div>
-//         </form>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Appointment;
