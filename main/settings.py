d63157d20df83893b286840f00d7f12b80fcad65
eshat002 <eshatjubayer22@outlook.com>
2024-11-23 20:41:59 +0600

    cofigure settings for djoser

--- main/settings.py
+++ main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "djoser",
     "rest_framework_simplejwt",
+    #"rest_framework_simplejwt.token_blacklist",
 ]
 
 AUTH_USER_MODEL = "account.UserAccount"
@@ -53,17 +54,43 @@ REST_FRAMEWORK = {
     ),
 }
 
+# AUTHENTICATION_BACKENDS = (
+#     "social_core.backends.google.GoogleOAuth2",
+#     "social_core.backends.facebook.FacebookOAuth2",
+#     "django.contrib.auth.backends.ModelBackend",
+# )
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
+    # "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    # "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    # 'AUTH_TOKEN_CLASSES': (
+    #     'rest_framework_simplejwt.tokens.AccessToken',
+    # )
 }
 
+ 
+
 DJOSER = {
-    "USER_CREATE_PASSWORD_RETYPE": True,
-    "SEND_ACTIVATION_EMAIL": False,
     "LOGIN_FIELD": "email",
+    "USER_CREATE_PASSWORD_RETYPE": True,
+    "USERNAME_CHANGED_EMAIL_CONFIRMATION": True,
+    "PASSWORD_CHANGED_EMAIL_CONFIRMATION": True,
+    "PASSWORD_RESET_CONFIRM_RETYPE": True,
+    "SEND_CONFIRMATION_EMAIL": True,
+    "SET_USERNAME_RETYPE": True,
+    "SET_PASSWORD_RETYPE": True,
+    "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
+    "USERNAME_RESET_CONFIRM_URL": "email/reset/confirm/{uid}/{token}",
+    "ACTIVATION_URL": "activate/{uid}/{token}",
+    "SEND_ACTIVATION_EMAIL": True,
+    "SOCIAL_AUTH_TOKEN_STRATEGY": "djoser.social.token.jwt.TokenStrategy",
+    "SOCIAL_AUTH_ALLOWED_REDIRECT_URIS": ["http://127.0.0.1:8000/google", "http://127.0.0.1:8000/facebook"],
     "SERIALIZERS": {
-        "user_create": "account.serializers.UserCreateSerializer",  # Optional customization
+        "user_create": "account.serializers.UserCreateSerializer",
+        "user": "account.serializers.UserCreateSerializer",
+        "current_user": "account.serializers.UserCreateSerializer",
+        "user_delete": "djoser.serializers.UserDeleteSerializer",
     },
 }
 
@@ -114,6 +141,12 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST_USER =  config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
