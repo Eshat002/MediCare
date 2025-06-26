0646f43934ab44599f8afdbe75f3c4e969b1bc6f
Eshat <eshatjubayer22@outlook.com>
2025-06-26 22:16:14 +0600

    fix SubscriptionCreateAPIView

--- newsletter/api/views.py
+++ newsletter/api/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
 from ..models import Subscription
 from .serializers import SubscriptionSerializer
-
+from rest_framework.permissions import AllowAny
 
 class SubscriptionCreateAPIView(generics.CreateAPIView):
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
+    permission_classes = [AllowAny]
