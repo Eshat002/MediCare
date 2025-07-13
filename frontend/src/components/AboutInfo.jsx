4ac3d9f2d3a9d71ff63d00c9d65e6dc2b49bd7b3
Eshat <eshatjubayer22@outlook.com>
2025-07-13 13:40:57 +0600

    add AboutInfo component to about us page

---
+++ frontend/src/components/AboutInfo.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { BiSolidQuoteSingleLeft } from "react-icons/bi";
+import { BiSolidQuoteSingleRight } from "react-icons/bi";
+
+const AboutInfo = () => {
+  return (
+    <section className="mt-20 pt-10 pb-28">
+      <div className="container mx-auto justify-center lg:px-24 px-6 max-w-4xl">
+        <BiSolidQuoteSingleLeft color="skyblue" size={28} />
+        <p>
+          Nullam sodales bibendum nulla ut vulputate. Vivamus auctor tincidunt
+          urna, ac molestie lectus imperdiet id. Maecenas nisl massa, mollis sed
+          sem a, finibus dapibus dui. Phasellus rhoncus, quam id molestie
+          efficitur, elit ipsum blandit diam, ac lobortis lectus nibh quis
+          ligula. Ut nunc erat, lobortis vel ultrices aliquet, efficitur id
+          mauris. Duis posuere hendrerit est lacinia consectetur. Morbi nec
+          volutpat ipsum. Cras blandit efficitur velit ut consequat. Suspendisse
+          potenti.
+        </p>
+        <p>
+          Quisque vitae nisi tempor, ultricies purus et, maximus metus.
+          Phasellus tempus a lorem vel ultricies. Pellentesque a pharetra
+          tellus, sit amet blandit ante. Morbi sollicitudin lacus ut orci
+          fermentum, in vestibulum nulla egestas. Pellentesque nisi orci,
+          ullamcorper ac bibendum quis, congue eget tellus. In sed est dolor.
+          Mauris gravida sem diam, et placerat metus lacinia non. Morbi
+          pellentesque ultricies ante at ultricies. Suspendisse vulputate elit
+          vel sapien volutpat pharetra. In hac habitasse platea dictumst.
+        </p>
+        <p>
+          Integer felis lorem, cursus ac leo quis, tristique elementum dolor. In
+          hac habitasse platea dictumst. In hac habitasse platea dictumst. Sed
+          interdum tincidunt suscipit. Sed sit amet tincidunt libero. Praesent
+          id condimentum lacus. Vivamus congue scelerisque pulvinar.
+        </p>
+        <BiSolidQuoteSingleRight />
+      </div>
+    </section>
+  );
+};
+
+export default AboutInfo;
--- frontend/src/screens/AboutPage.jsx
+++ frontend/src/screens/AboutPage.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import PageTitleCard from "../components/PageTitleCard";
+import AboutInfo from "../components/AboutInfo";
 
 const AboutPage = () => {
   return (
-    <div>
-      <PageTitleCard
-        title="About Us"
-        image="https://images.pexels.com/photos/6129507/pexels-photo-6129507.jpeg"
-        imagePosition={"center top"}
-      />
-    </div>
+    <>
+      <div>
+        <PageTitleCard
+          title="About Us"
+          image="https://images.pexels.com/photos/6129507/pexels-photo-6129507.jpeg"
+          imagePosition={"center top"}
+        />
+      </div>
+      <div>
+        <AboutInfo />
+      </div>
+    </>
   );
 };
 
